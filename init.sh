e90fae2243cdbdc1b97617973f11785db66b2c0f
Tilo Ullrich <dev@tilo-ullrich.de>
2021-02-14 17:53:02 +0100

    fixed init.sh script

--- init.sh
+++ init.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
-for i in $(ls -a dotfiles); do
-    unlink ~/$i
+
+cd $(dirname $0)
+
+for i in $(ls -a dotfiles | grep -v "\.$"); do
+    rm -Rf ~/$i
 done
 
-for i in $(ls -a dotfiles); do
+for i in $(ls -a dotfiles | grep -v "\.$"); do
     ln -s $(pwd)/dotfiles/$i ~
 done
+
+cd - > /dev/null
